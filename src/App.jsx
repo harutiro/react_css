bba8723a1a1f76cfee549b74aa77e7e1612c2c10
harutiro <harutiro2027@outlook.jp>
2022-12-02 10:06:22 +0900

    update: jsxでスタイルを指定してみる。

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import { useState } from "react";
 // import { useEffect } from "react";
-import { CssModules } from "./components/cssModules";
+// import { CssModules } from "./components/cssModules";  
+import { StyledJsx } from "./components/StyledJsx";
 
 export const App = () => {
     return (
         <>
-            <CssModules />
+            {/* <CssModules /> */}
+            <StyledJsx />
         </>
     );
 };
